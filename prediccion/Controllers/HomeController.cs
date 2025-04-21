024d283658c1846c575024e9d4b7b66019ad5cc1
Diego Cuaycal <diegocuaycal01@gmail.com>
2025-04-20 23:26:18 -0500

    Decoración visual del proyecto: mejoras en navbar, inicio, predicción y pie de página.

--- prediccion/Controllers/HomeController.cs
+++ prediccion/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Inicio()
+    {
+        return View();
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
